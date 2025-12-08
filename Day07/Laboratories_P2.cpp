73280ecca1396f823048d0137592c07442a7780d
darkfalc0n <pratyayroy20@gmail.com>
2025-12-08 23:59:26 +0530

    Day07

---
+++ Day07/Laboratories_P2.cpp
@@ -0,0 +1,146 @@
+/**
+ *   Advent of Code Template by DarkFalcon (Pratyay Roy)
+ *   https://adventofcode.com
+ *   Solved on: 2025-12-07
+ */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+#define endl '\n'
+#define _ << ' ' <<
+#define F first
+#define S second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define int long long
+#define nl cout << endl;
+#define SZ(A) (int)(A.size())
+#define SUM(A) accumulate(all(A), 0LL)
+#define print(...) cout << __VA_ARGS__ << endl
+#define YN(f) cout << (f ? "YES" : "NO") << endl
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define LB(A, x) distance((A).begin(), lower_bound(all(A), (x)))
+#define UB(A, x) distance((A).begin(), upper_bound(all(A), (x)))
+#define UNQ(A) sort(all(A)), A.erase(unique(all(A)), A.end()), A.shrink_to_fit()
+#define all(A) A.begin(), A.end()
+#define rall(A) A.rbegin(), A.rend()
+#define SOR(A) sort(all(A))
+#define RSOR(A) sort(rall(A))
+#define REV(A) reverse(all(A))
+#define MAX(A) *max_element(all(A))
+#define MIN(A) *min_element(all(A))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using i128 = __int128;
+using f128 = __float128;
+using ld = long double;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vc = vector<char>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+using vvi = vector<vi>;
+using vvb = vector<vb>;
+using vvc = vector<vc>;
+using vvs = vector<vs>;
+using vpii = vector<pii>;
+
+const int mod = 1000000007;
+const int mod2 = 998244353;
+const int INF32 = 1'010'000'000;
+const ld PI = 3.1415926535897932384626;
+const int INF64 = 2'020'000'000'000'000'000;
+
+static auto __fast_io = []()
+{ ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
+
+int binToDec(string s) { return bitset<64>(s).to_ullong(); }
+string decToBin(int a) { return bitset<64>(a).to_string(); }
+
+class Solution
+{
+private:
+    int paths(vs &arr, int origin, int index, vvi &dp)
+    {
+        if (dp[index][origin] >= 0)
+            return dp[index][origin];
+
+        if (index == arr.size() - 1)
+        {
+            if (arr[index][origin] == '^')
+                return dp[index][origin] = 2;
+            return dp[index][origin] = 1;
+        }
+
+        if (arr[index][origin] == '^')
+            return dp[index][origin] = paths(arr, origin + 1, index + 1, dp) + paths(arr, origin - 1, index + 1, dp);
+        return dp[index][origin] = paths(arr, origin, index + 1, dp);
+    }
+
+public:
+    // use `in >> x` for input
+    void solve(istream &in = cin, ostream &out = cout)
+    {
+        string s;
+        getline(in, s);
+        int n = s.size(), origin;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == 'S')
+            {
+                origin = i;
+            }
+        }
+
+        getline(in, s);
+
+        vs arr;
+        while (getline(in, s))
+        {
+            arr.pb(s);
+            getline(in, s);
+        }
+
+        vvi dp(arr.size(), vector<int>(n, -1));
+
+        cout << paths(arr, origin, 0, dp) << endl;
+    }
+};
+
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    ifstream fin;
+    istream *inPtr = &cin;
+    vector<string> tries = {"./input.txt", "input.txt", "../input.txt"};
+
+    for (string &p : tries)
+    {
+        fin.open(p);
+        if (fin)
+        {
+            inPtr = &fin;
+            break;
+        }
+    }
+
+    if (inPtr == &cin)
+    {
+        cerr << "Failed to open input.txt\n";
+        return 1;
+    }
+
+    Solution sol;
+    sol.solve(*inPtr);
+
+    return 0;
+}
